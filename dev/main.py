da0d81e87abaae3d78968dcc2cda88109995ab0e
Matias Vazquez <mfvazquez@fi.uba.ar>
2019-09-01 23:17:08 -0300

    adding words from csv files

--- dev/main.py
+++ dev/main.py
@@ -4,13 +4,28 @@ from psychopy import core, visual, monitors
 import json
 import stim
 import sys
+import csv
+import os.path
+
+
+def csv_read(file, delimiter):
+    with open(file) as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=delimiter)
+        csv_list = list(readCSV)
+    return csv_list
 
 
 if __name__ == "__main__":
 
-    with open("../data/paradigm.json") as paradigm_config:
+    # Path to this file
+    script_path = os.path.abspath(os.path.dirname(__file__))
+
+    # Loads paradigm configuration
+    paradigm_file = os.path.join(script_path, "../data/paradigm.json")
+    with open(paradigm_file) as paradigm_config:
         config = json.load(paradigm_config)
 
+    # Opens the window
     if "window" in config:
         print("Loading custom setup...")
         setup = config["window"]
@@ -19,6 +34,7 @@ if __name__ == "__main__":
         print("Loading default setup...")
         win = visual.Window(monitor='testMonitor', units='deg')
 
+    # Creates the stimulus
     stimuli = []
     for stimulus_conf in config["trial"]:
         stimulus = stim.Constructor.create(stimulus_conf, win)
@@ -29,17 +45,29 @@ if __name__ == "__main__":
     print("{0} stimulus created successfully".format(len(stimuli)))
     print("Loading words sequence...")
 
-    for stimulus in stimuli:
+    # Loads the words
+    blocks = []
+    for block_file in config["sequence"]:
+        words_file = os.path.join(script_path, "../data/", block_file)
+        blocks.append(csv_read(words_file, ','))
 
-        if stimulus.needs_update():
-            stimulus.update("some word")
+    # Runs the paradigm
+    block_number = 1
+    for block in blocks:
+        print("running block {0}...".format(x))
+        for (word, word_code) in block:
 
-        if not stimulus.run():
-            print("Exit key pressed. Leaving application...")
-            win.close()
-            core.quit()
-            sys.exit()
+            for stimulus in stimuli:
 
+                if stimulus.needs_update():
+                    stimulus.update(word)
+
+                if not stimulus.run():
+                    print("Exit key pressed. Leaving application...")
+                    win.close()
+                    core.quit()
+                    sys.exit()
+        block_number += 1
 
     win.close()
     core.quit()
