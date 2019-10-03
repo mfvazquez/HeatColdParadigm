f046259ae77657e008e8aaf8fe78973257644b04
Matias <mfvazquez@fi.uba.ar>
2019-10-03 19:57:00 -0300

    Updating installation script

--- install/setup.sh
+++ install/setup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-pip3 install -r $(dirname $0)/requirements
+install_dir=$(dirname $0)
+pip install -r $install_dir/requirements
+whl_file=wxPython-4.0.6-cp36-cp36m-linux_x86_64.whl
+wget https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-18.04/$whl_file -O $install_dir/$whl_file 
 sudo apt-get install libusb-1.0-0-dev portaudio19-dev libasound2-dev
-pip3 install $(dirname $0)/wxPython-4.0.6-cp36-cp36m-linux_x86_64.whl
+pip install $install_dir/$whl_file
